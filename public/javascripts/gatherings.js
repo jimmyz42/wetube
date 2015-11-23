e6ed0b3ed62ea1738af6e1fa48d4521b39500be6
Dhroova Aiylam <dhroova@mit.edu>
2015-11-23 18:57:13 -0500

    fixed javascript stuff

--- public/javascripts/gatherings.js
+++ public/javascripts/gatherings.js
@@ -1,48 +1,15 @@
-////TODO: Change instances of "gatheringInfo" to match actual server data
-
 (function() {
 
-	var loadGatheringPage = function() {
-		$.get('/mygathering'
-		).done(function(response) {
-			loadPage('gathering', { gathering: response.content.gathering, currentUser: currentUser })
-		}).fail(function(responseObject) {
-			var error = $.parseJSON(responseObject.responseText);
-			loadPage('error', {currentUser : currentUser, error : error});
-		});
-	};
-		
-	var loadJoinGatheringPage = function() {
-		if(currentUser)
-		{
-			loadPage('joinGathering', {currentUser : currentUser});
-		}
-		else
-		{
-			loadPage('error', {currentUser : currentUser});
-		}
-	};
-
-	var loadCreateGatheringPage = function() {
-		$.get('/gathering'
-		).done(function(response) {
-			loadPage('createGathering', { shoutkey: response.content.shoutkey, currentUser: currentUser });	
-		}).fail(function(responseObject) {
-			var error = $.parseJSON(responseObject.responseText);
-			loadPage('error', {currentUser : currentUser, error : error});
-		});
-	};
-
 	$(document).on('click', '#mygathering-btn', function(evt) {
-		loadGatheringPage();
+		window.location = '/gathering';
 	});
 
 	$(document).on('click', '#join-btn', function(evt) {
-		loadJoinGatheringPage();
+		window.location = '/joinGathering';
 	});
 
 	$(document).on('click', '#gatherings-btn', function(evt) {
-		loadCreateGatheringPage();
+		window.location = '/createGathering';
 	});
 
 	$(document).on('click', '#creategathering-btn', function(evt) {
@@ -52,7 +19,7 @@
 			{name : helpers.getFormData(this).name,
 			 shoutkey : $("#gatheringName").html()} 
 		).done(function(response) {
-			loadGatheringPage();
+			window.location = '/gathering';
 		}).fail(function(responseObject) {
 			var error = $.parseJSON(responseObject.responseText);
 			loadPage('error', {currentUser : currentUser, error : error});
@@ -60,41 +27,31 @@
 	});
 
 	$(document).on('click', '#joingathering-btn', function(evt) {
-		evt.preventDefault();
 		var shoutkey = helpers.getFormData(this).shoutkey;
-		$.ajax({
-			  url: '/gathering/' + shoutkey,
-			  type: 'GET'
-		  }).done(function(response) {
-			loadPage('gathering', {gatheringInfo : response.content.gatheringInfo}) //TODO : fill in
-		}).fail(function(responseObject) {
-			var response = $.parseJSON(responseObject.responseText);
-			$('.error').text(response.err);
-		});
+		window.location = '/gathering/' + shoutkey;
 	});
 
 	$(document).on('click', '#delete-gathering', function(evt) {
 		  var gathering = $(this).parent();
 		  var shoutkey = gathering.data('shoutkey');
-		  $.ajax({
-			  url: '/gathering/' + shoutkey,
-			  type: 'DELETE'
+		  $.delete({
+			'/gathering/' + shoutkey
 		  }).done(function(response) {
-			  item.remove();
+			  window.location = '/homepage';
 		  }).fail(function(responseObject) {
-			  var error = $.parseJSON(responseObject.responseText);
-			  loadPage('error', {currentUser : currentUser, error : error});
+			  var response = $.parseJSON(responseObject.responseText);
+			  $('.error').text(response.err);
 		  });
 	  });
 
 	$(document).on('click', '#members-btn', function(evt) {
 		$.get(
-			'/mygathering'
+			'/gathering'
 		).done(function(response) {
-			loadPage('members', {gatheringInfo : response.content.gatheringInfo}) //TODO : fill in
+			window.location = '/members';
 		}).fail(function(responseObject) {
-			var error = $.parseJSON(responseObject.responseText);
-			loadPage('error', {currentUser : currentUser, error : error});
+			var response = $.parseJSON(responseObject.responseText);
+			$('.error').text(response.err);
 		});
 	});
 
