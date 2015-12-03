16aac4e57e9993f465bbcc5ea12aabfc2d412c3c
Miri Choi <mirichoi@mit.edu>
2015-12-03 06:46:59 -0500

    ui changes

---
+++ public/images/BoarderImage_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "CAMBRIDGE",
+      'continent': "NA",
+      'country': "US",
+      'region': "MA"
+    },
+    'ip':"18.189.98.245"
+  }]);
+})
+//
+()
+
+;
